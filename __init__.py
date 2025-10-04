c0acab51633624ea3126aaf2c8a4a9e0e7f4c354
PGCRT <plugcrypt@gmail.com>
2025-10-04 22:13:49 +0200

    push

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 """
 @author: CRT
 @title: CRT-Nodes
-@version: 1.7.8
+@version: 1.7.9
 @project: "https://github.com/plugcrypt/CRT-Nodes",
 @description: Set of nodes for ComfyUI
 https://discord.gg/8wYS9MBQqp
