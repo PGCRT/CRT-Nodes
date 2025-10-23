5dea08f2af0062f5973788cca10b5e22d1312417
PGCRT <plugcrypt@gmail.com>
2025-10-23 20:15:49 +0200

    SyntaxError fix for some python version

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 """
 @author: CRT
 @title: CRT-Nodes
-@version: 1.8.4
+@version: 1.8.5
 @project: "https://github.com/plugcrypt/CRT-Nodes",
 @description: Set of nodes for ComfyUI
 https://discord.gg/8wYS9MBQqp
