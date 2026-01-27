1ab9a860c3a7da0bb3afed63878cc4d0ea950420
PGCRT <plugcrypt@gmail.com>
2026-01-27 17:12:35 +0100

    ultralytics no deps

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 """
 @author: CRT
 @title: CRT-Nodes
-@version: 2.1.3
+@version: 2.1.4
 @project: "https://github.com/plugcrypt/CRT-Nodes",
 @description: Set of nodes for ComfyUI
 https://discord.gg/8wYS9MBQqp
