60607e98c11624323e37b8d24fddcc5936e412a3
PGCRT <plugcrypt@gmail.com>
2026-03-16 17:38:12 +0100

    2.1.8 add prompts

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 """
 @author: CRT
 @title: CRT-Nodes
-@version: 2.1.7
+@version: 2.1.8
 @project: "https://github.com/plugcrypt/CRT-Nodes",
 @description: Set of nodes for ComfyUI
 https://discord.gg/8wYS9MBQqp
